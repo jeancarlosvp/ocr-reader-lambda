8604674d3a8c9ae3adf63f0f6f511b2a6d6451cd
Jean Vargas <carlosvp0324@gmail.com>
2023-11-30 19:10:17 -0500

    deploy heroku

--- app/payments/routers.py
+++ app/payments/routers.py
@@ -2,14 +2,13 @@ from fastapi import APIRouter, Request, status, UploadFile
 from typing import Union
 from app.security import valid_header
 from app.config import API_KEY, SEND_GS
-from app.config import worksheet
-from app.payments.services import get_row
+
 from app.payments.utils import (
     process_uploaded_file,
     select_bank,
-    get_bank_data,
-    update_worksheet
+    get_bank_data
 )
+import requests
 
 router = APIRouter(prefix="/payments", tags=["payments"])
 
@@ -19,7 +18,6 @@ async def upload_payment_file(file: Union[UploadFile, None], request: Request):
         valid_header(request, API_KEY)
         if not file:
             return {"message": "No file uploaded"}
-        row_to_add = get_row(worksheet)
         result_list = process_uploaded_file(file)
         banks = ["INTERBANK", "BBVA", "SCOTIABANK", "BANBIF"]
         selection = select_bank(result_list, banks)
@@ -27,8 +25,18 @@ async def upload_payment_file(file: Union[UploadFile, None], request: Request):
         print(matches_dict)
         to_send = SEND_GS == "True"
         if to_send:
-            update_worksheet(worksheet, row_to_add, matches_dict)
-
-        return {'mensaje': 'Imagen subida satisfactoriamente', 'se agregó en la fila': row_to_add, 'datos': matches_dict}
+            data_payment = {
+                "date_payment": "2021-09-01",
+                "dni": matches_dict.get("DNI", ""),
+                "total": matches_dict.get("Total", ""),
+                "bank": matches_dict.get("Banco", ""),
+                "mode_payment": "DS",
+                "operation_code": matches_dict.get("Codigo de operacion", ""),
+                "card_number": matches_dict.get("Numero tarjeta", "")
+            }
+            res = requests.post("https://sender-data-gss-f511a3058b22.herokuapp.com/datapayments/", json=data_payment)
+            if res.status_code != 201:
+                return {"message": "Error sending data to google sheet"}
+            return {'mensaje': 'Imagen subida satisfactoriamente', 'datos': matches_dict}
     except Exception as e:
          return {'message': 'An error occurred', 'error': str(e)}
--- app/services.py
+++ app/services.py
@@ -1,11 +1,11 @@
-import gspread
+# import gspread
 import easyocr as ocr
 
-def get_worksheet(credentials, book_id, sheet_name):
-    gc = gspread.service_account_from_dict(credentials)
-    book = gc.open_by_key(book_id)
-    worksheet = book.worksheet(sheet_name)
-    return worksheet
+# def get_worksheet(credentials, book_id, sheet_name):
+#     gc = gspread.service_account_from_dict(credentials)
+#     book = gc.open_by_key(book_id)
+#     worksheet = book.worksheet(sheet_name)
+#     return worksheet
 
 def get_ocr_model():
     reader = ocr.Reader(['en'],model_storage_directory='.')
